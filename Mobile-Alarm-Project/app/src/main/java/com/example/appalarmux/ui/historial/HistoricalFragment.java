6255b9c368e76e07d1e3ee2861873078fc16f680
Santiago Alarcon <s.alarcong@uniandes.edu.co>
2024-09-22 16:51:42 -0500

    Se agregan efectos para la elimianción de alarmas en el histórico

--- Mobile-Alarm-Project/app/src/main/java/com/example/appalarmux/ui/historial/HistoricalFragment.java
+++ Mobile-Alarm-Project/app/src/main/java/com/example/appalarmux/ui/historial/HistoricalFragment.java
@@ -1,11 +1,14 @@
 package com.example.appalarmux.ui.historial;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -32,36 +35,136 @@ public class HistoricalFragment extends Fragment {
         binding = FragmentHistorialBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // Obtener el ImageView por el ID
-        ImageView imageViewB = binding.imageViewB; // Asegúrate de que el ID coincida con tu XML
+        ImageView imageViewB = binding.imageViewB;
+        ImageView imageView = binding.imageView;
         ImageView imageViewDeleted = binding.imageButtonDeleteB;
-        // Establecer el listener para modificar las propiedades del layout
+        ImageView imageButtonDeleteB2 = binding.imageButtonDeleteB2;
+        TextView textView2B = binding.textView2B;
+        TextView textView3B = binding.textView3B;
+        TextView textView5B = binding.textView5B;
+        TextView textViewPM1B = binding.textViewPM1B;
+        ImageView imageViewPM1B = binding.imageViewPM1B;
+        TextView textView2 = binding.textView2;
+        TextView textView3 = binding.textView3;
+        TextView textView5 = binding.textView5;
+        TextView textViewPM1 = binding.textViewPM1;
+        ImageView imageViewPM1 = binding.imageViewPM1;
+        ImageButton imageButtonDeleteB = binding.imageButtonDeleteB;
+        ImageButton deleteButton = binding.imageButtonDeleteB;
+        ImageButton backButton = binding.backButtom;
+
         imageViewB.setOnClickListener(v -> {
-            // Obtener el layout que contiene el ImageView
-            ConstraintLayout layout = (ConstraintLayout) v.getParent();
+            int visibilityValue= imageButtonDeleteB.getVisibility();
+            if (visibilityValue == 0){
+                ConstraintLayout layout = binding.constraintLayout;
+                ConstraintSet constraintSet = new ConstraintSet();
+                constraintSet.clone(layout);
+                constraintSet.constrainWidth(imageViewB.getId(), 1200);
+                constraintSet.setHorizontalBias(imageViewB.getId(), 0.5f);
+                constraintSet.applyTo(layout);
+                deleteButton.setVisibility(View.GONE);
 
-            // Crear un nuevo ConstraintSet para modificar las propiedades del layout
-            ConstraintSet constraintSet = new ConstraintSet();
-            constraintSet.clone(layout); // Clonar el layout actual
-
-            // Modificar el ancho del ImageView
-            constraintSet.constrainWidth(v.getId(), 1100); // Establecer el ancho en 350
-
-            // Mantener el valor de layout_constraintHorizontal_bias
-            //float bias = constraintSet.getHorizontalBias(v.getId());
-            constraintSet.setHorizontalBias(v.getId(), 0.31f); // Reaplicar el bias actual
-            // Aplicar los cambios
-            constraintSet.applyTo(layout);
-            // Hacer visible el ImageView imageViewDeleteB
-            imageViewDeleted.setVisibility(View.VISIBLE);
+            }else{
+                ConstraintLayout layout = (ConstraintLayout) v.getParent();
+                ConstraintSet constraintSet = new ConstraintSet();
+                constraintSet.clone(layout);
+                constraintSet.constrainWidth(imageViewB.getId(), 1100);
+                constraintSet.setHorizontalBias(imageViewB.getId(), 0.31f);
+                constraintSet.applyTo(layout);
+                imageViewDeleted.setVisibility(View.VISIBLE);
+            }
         });
 
-        // Configurar el botón back_button para regresar al fragmento fragment_home
-        ImageButton backButton = binding.backButtom; // Asegúrate de que el ID coincida con tu XML
+        imageView.setOnClickListener(v -> {
+            int visibilityValue= imageButtonDeleteB2.getVisibility();
+            if (visibilityValue == 0){
+                ConstraintLayout layout = binding.constraintLayout;
+                ConstraintSet constraintSet = new ConstraintSet();
+                constraintSet.clone(layout);
+                constraintSet.constrainWidth(imageView.getId(), 1200);
+                constraintSet.setHorizontalBias(imageView.getId(), 0.5f);
+                constraintSet.applyTo(layout);
+                imageButtonDeleteB2.setVisibility(View.GONE);
+
+            }else{
+                ConstraintLayout layout = (ConstraintLayout) v.getParent();
+                ConstraintSet constraintSet = new ConstraintSet();
+                constraintSet.clone(layout);
+                constraintSet.constrainWidth(imageView.getId(), 1100);
+                constraintSet.setHorizontalBias(imageView.getId(), 0.31f);
+                constraintSet.applyTo(layout);
+                imageButtonDeleteB2.setVisibility(View.VISIBLE);
+            }
+        });
+
         backButton.setOnClickListener(v -> {
             NavController navController = Navigation.findNavController(v);
             navController.navigate(R.id.navigation_home);
         });
+
+
+        deleteButton.setOnClickListener(v -> {
+            new AlertDialog.Builder(requireContext())
+                    .setTitle("Eliminar alarma")
+                    .setMessage("¿Estás seguro de que quieres eliminar esta alarma?")
+                    .setPositiveButton("Sí", (dialog, which) -> {
+
+                        textView2B.setVisibility(View.GONE);
+                        textView3B.setVisibility(View.GONE);
+                        textView5B.setVisibility(View.GONE);
+                        textViewPM1B.setVisibility(View.GONE);
+                        imageViewPM1B.setVisibility(View.GONE);
+                        imageButtonDeleteB.setVisibility(View.GONE);
+                        imageViewB.setVisibility(View.GONE);
+                        Toast.makeText(requireContext(), "Alarma eliminada", Toast.LENGTH_SHORT).show();
+                    })
+                    .setNegativeButton("No", (dialog, which) -> {
+
+                        ConstraintLayout layout = binding.constraintLayout;
+                        ConstraintSet constraintSet = new ConstraintSet();
+                        constraintSet.clone(layout);
+                        constraintSet.constrainWidth(imageViewB.getId(), 1200);
+                        constraintSet.setHorizontalBias(imageViewB.getId(), 0.5f);
+                        constraintSet.applyTo(layout);
+                        deleteButton.setVisibility(View.GONE);
+
+                        dialog.dismiss();
+                    })
+                    .create()
+                    .show();
+        });
+
+        imageButtonDeleteB2.setOnClickListener(v -> {
+            new AlertDialog.Builder(requireContext())
+                    .setTitle("Eliminar alarma")
+                    .setMessage("¿Estás seguro de que quieres eliminar esta alarma?")
+                    .setPositiveButton("Sí", (dialog, which) -> {
+
+                        textView2.setVisibility(View.GONE);
+                        textView3.setVisibility(View.GONE);
+                        textView5.setVisibility(View.GONE);
+                        textViewPM1.setVisibility(View.GONE);
+                        imageViewPM1.setVisibility(View.GONE);
+                        imageButtonDeleteB2.setVisibility(View.GONE);
+                        imageView.setVisibility(View.GONE);
+                        Toast.makeText(requireContext(), "Alarma eliminada", Toast.LENGTH_SHORT).show();
+                    })
+                    .setNegativeButton("No", (dialog, which) -> {
+
+                        ConstraintLayout layout = binding.constraintLayout;
+                        ConstraintSet constraintSet = new ConstraintSet();
+                        constraintSet.clone(layout);
+                        constraintSet.constrainWidth(imageView.getId(), 1200);
+                        constraintSet.setHorizontalBias(imageView.getId(), 0.5f);
+                        constraintSet.applyTo(layout);
+                        imageButtonDeleteB2.setVisibility(View.GONE);
+
+                        dialog.dismiss();
+                    })
+                    .create()
+                    .show();
+        });
+
 
         return root;
     }
